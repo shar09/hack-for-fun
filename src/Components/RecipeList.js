fee19531eff7a8b4e5fbb6738d2ee748a75e4dd3
fancyaction <labcoatdisaster@gmail.com>
2019-11-06 15:20:25 -0600

    add syled components, add RecipeContainer

--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import RecipeCard from './RecipeCard';
+import styled from 'styled-components';
+
+const RecipeContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+`;
 
 const RecipeList = ({ recipes }) => (
-    <div>
+    <RecipeContainer>
         {recipes.map((recipe, index) => (
             <RecipeCard key={index} imageSrc={recipe.imageSrc} title={recipe.title} content={recipe.content} link={recipe.link} />
         ))}
-    </div>
+    </RecipeContainer>
 );
 
 export default RecipeList;
