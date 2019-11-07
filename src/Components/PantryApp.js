684cb82376a63f732b2056c506eaa0bb73fd8a43
shar09 <sharathtelu9@gmail.com>
2019-11-07 09:12:55 -0600

    Main page styled

--- src/Components/PantryApp.js
+++ src/Components/PantryApp.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import RecipeList from './RecipeList';
+import './PantryApp.css';
 
 const PantryApp = props => {
     const [recipes, setRecipes] = React.useState([{ imageSrc: 'test', title: 'test', content: 'test', link: 'test' }]);
     console.log("TCL: recipes", recipes)
 
     return (
-        <div>
-            <h1>Welcome to the pantry!</h1>
+        <div className="root">
+        <div className="wrappper">
+            <h1 className="header">Welcome to the pantry!</h1>
             {recipes && <RecipeList recipes={recipes} />}
             {/* IngredientsForm */}
+
+            <div className="push"></div>
         </div>
+        
+        <footer>
+            Footer 2019. 
+        </footer> 
+        </div>     
     );
 };
 
