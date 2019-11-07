360409febe23030364be4c511ad5e76e3e61f21e
thedeadline <igiuong@gatech.edu>
2019-11-07 09:24:32 -0600

    Add input

--- dw2019/src/App.js
+++ dw2019/src/App.js
@@ -3,24 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  constructor(props) {
+   super(props);
+   this.state = {
+     word: '',
+     items: []
+   };
+ }
+
+ addToList = (e) => {
+   this.setState({ items: [this.state.word, ...this.state.items], word: ''})
+   this.renderList();
+
+ }
+
+ onChange = (e) => {
+   this.setState({word: e.target.value});
+ }
+ 
+ render() {
+     const renderList = () => {
+       return (
+     <ul>
+       {this.state.items.map((word, index) =>
+         <li key={index}>{word}</li>) }
+     </ul>
+   )};
+
+   return <div>
+     <input value={this.state.word} onChange={this.onChange} />
+     <button onClick={this.addToList}>+</button>
+     {renderList()}
+   </div>;
+ }
 
 export default App;
